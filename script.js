aeda39f81fb6b089c3d5b7862d44615377793231
Petro Maltsev <ph62pse@eduvaud.ch>
2026-03-10 17:10:02 +0100

    Intro JS

--- script.js
+++ script.js
@@ -20,3 +20,53 @@
         updateCarousel();
       });
     });
+
+// Contact
+document.addEventListener('DOMContentLoaded', function () {
+const form = document.getElementById("form-contact");
+const res = document.getElementById("section-res");
+const mainTitle = document.getElementById("h1");
+const description = document.querySelector(".p");
+
+let surnameInput = document.getElementById("surname");
+let nameInput = document.getElementById("name");
+let cityInput = document.getElementById("city");
+let adresseInput = document.getElementById("street");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+
+  const p = document.createElement("p");
+
+
+  const paraName = document.createElement("p");
+  paraName.textContent = `Nom: ${nameInput.value}`;
+
+  const paraSurname = document.createElement("p");
+  paraSurname.textContent = `Prénom: ${surnameInput.value}`;
+
+  const paraCity = document.createElement("p");
+  paraCity.textContent = `Lieu: ${cityInput.value}`;
+
+  const div = document.createElement("div");
+  div.id = "result";
+
+
+  mainTitle.textContent = "Vos coordonnées";
+
+  description.remove();
+    
+
+  div.appendChild(paraName);
+  div.appendChild(paraSurname);
+  div.appendChild(paraCity);
+
+  form.remove();
+  
+
+  res.appendChild(div);
+
+   form.reset();
+});
+});
